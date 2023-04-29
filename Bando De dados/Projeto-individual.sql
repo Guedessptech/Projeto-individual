f6111247d0ba998081006f568abb8fc472b391a4
guedessptech <gabriel.gsantos@sptech.school>
2023-04-28 21:39:25 -0300

    Atualização do der e scrip

---
+++ Bando De dados/Projeto-individual.sql
@@ -0,0 +1,20 @@
+create database projeto;
+use projeto;
+create table tbformulario(
+idformulario int primary key auto_increment,
+nomePL varchar(45),
+equipePl varchar(45)
+);
+
+
+insert into tbformulario value 
+(null, 'Hamilton','Mercedes'),
+(null,'Verstappen','Red Bull'),
+(null, 'Leclerc','Ferrari');
+
+
+
+
+
+
+
